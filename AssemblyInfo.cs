f1d983bd201544a6b117951fcda5f2256380da0c
Asoji <asoji-git@hiitsdevin.mozmail.com>
2024-09-03 00:33:27 -0700

    🔧 added assemblyinfo.cs

---
+++ AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Source2Tools-DRPC")]
+[assembly: AssemblyDescription("Attach Discord Rich Presence to your Source 2 Tools you're running")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("asojidev")]
+[assembly: AssemblyProduct("Source2Tools-DRPC")]
+[assembly: AssemblyCopyright("©️ asojidev. MIT Licensed.")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
